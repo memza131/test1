6acaf41e388d2a6e11e826a6a799bab84d99ce73
memza131 <memza131@gmail.com>
2021-07-01 14:02:19 +0700

    first commit

---
+++ test1.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.write(' ---')
+
+st.write(""" ** Welcome to  my demo model. ** """)
+
+
+st.write(''' - - - - ''')
+
+st.write('''# `what is `''')
+
+st.write(''' - - - - ''')
+
+st.write('''`bababababa`''')
+
+st.write('')
